efcde1eaabe6430512aae3762b2ae866cbcdd9fb
cpbahamonde <cpbahamonde@gmail.com>
2022-03-13 14:29:17 -0300

    actualizacion avance 20220313 14.30

---
+++ assets/js/app.js
@@ -0,0 +1,79 @@
+
+// SMOOTH SCROLL
+
+    $(document).ready(function () {
+        $("a").click( function() { 
+            var gato = this.hash
+            $("html, body").animate(
+                {
+                        scrollTop: $(gato).offset().top - 40
+                    }, 800
+            )
+        })
+    });
+
+
+// BOTON "Enviar" --> ENVIA MENSAJE POR PANTALLA
+    function MsjEnv() { alert("El mensaje fue enviado correctamente..."); }
+
+    $(document).ready(function () {
+        $("#enviarMsj").tooltip({ placement: "bottom" });
+    });
+
+// OCULTA/MUESTRA TEXTO EN SECCION QSomos
+    $(document).ready(function () {
+        $(".icon-title").click(function () {
+            $(".qsomos-txt").toggle();
+        });
+    });
+
+// OCULTA/MUESTRA TEXTO EN SECCION Destacados
+    $(document).ready(function () {
+        $(".card-img").click(function () {
+            $(".card-text").toggle();
+        });
+    });
+
+jQuery(document.links).filter(function () {
+    return this.hostname != window.location.hostname;
+}).attr('target', '_blank');
+
+
+
+
+
+
+
+
+// NAVBAR CAMBIO COLOR
+// fuente:  https://es.stackoverflow.com/questions/95528/navbar-scroll-que-cambie-de-color-al-pasar-de-secci%C3%B3n
+
+$(function (){
+      $(window).scroll(function(){
+         if ($(this).scrollTop() > 100) {
+          $('.navscroll').addClass("bnegra");
+         } else {
+          $(".navscroll").removeClass("bnegra");
+         }
+      });
+  });
+
+/*
+
+
+$(function (){
+      $(window).scroll(function(){
+         if ($(this).scrollTop() > 100) {
+          $('.navscroll').addClass("azul");
+         } else {
+          $(".navscroll").removeClass("azul");
+         }
+      });
+  });
+
+$(document).scroll(function(e) {
+    const y = $("html").scrollTop();
+    if (y > 300) $("nav").addClass("nav-black")
+    else $(nav).removeClass("nav-black")
+})
+*/
